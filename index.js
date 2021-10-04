e6cccd6a3eb6cc8b36aecea0cf8f39f0e1b53df7
songsh927 <songsh96927@gmail.com>
2021-10-04 17:37:25 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import http from 'http';
+import {Server} from 'socket.io';
+
+const app = express();
+
+app.get('/', function(req, res){
+		res.sendFile('/home/song/NODE/chatapp/index.html');
+		});
+
+const server = app.listen(3000);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+		console.log('user connected');
+
+		socket.on('chat message', (msg) => {
+				io.emit('chat message', msg);
+				});
+	
+		socket.on('disconnect', () => {
+				console.log('user disconnected');
+				});
+		
+		});
+
+
